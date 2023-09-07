c34a176111dd17a25322f5ca4c5ab3a26130202e
Valorando <vadimsokil.dnipro@gmail.com>
2023-09-07 12:42:54 +0300

    Realization.

---
+++ I_Provider_Factory_Wrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_07_09
+{
+    internal interface I_Provider_Factory_Wrapper
+    {
+        DbConnection CreateConnection();
+        string Connection_string { get; set; }
+    }
+}
